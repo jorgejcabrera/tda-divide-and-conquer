146d75e823f9a8b367f496553c4c84a305da6078
jorgejcabrera <cabrerajjorge@gmail.com>
2022-04-15 18:38:22 -0300

    v2 implementation of divide and conquer strategy ready

--- rank/sort_service.py
+++ rank/sort_service.py
@@ -1,30 +1,30 @@
 class SortService:
 
     def __init__(self):
-        self.movements_to_right = dict()
+        self.movements_to_right_v2 = dict()
 
-    def all_improvements_by_previous_position(self, lista):
+    def all_improvements_by_previous_position_v2(self, lista):
         if len(lista) > 1:
             half = len(lista) // 2
             left = lista[:half]
             right = lista[half:]
 
-            self.all_improvements_by_previous_position(left)
-            self.all_improvements_by_previous_position(right)
+            self.all_improvements_by_previous_position_v2(left)
+            self.all_improvements_by_previous_position_v2(right)
 
             i = 0
             j = 0
             k = 0
 
             while i < len(left) and j < len(right):
-                if left[i] < right[j]:
+                if left[i].previous_rank < right[j].previous_rank:
                     lista[k] = left[i]
                     i += 1
                 else:
-                    if left[i] in self.movements_to_right:
-                        self.movements_to_right[left[i]] += 1
+                    if left[i].name in self.movements_to_right_v2:
+                        self.movements_to_right_v2[left[i].name] += 1
                     else:
-                        self.movements_to_right[left[i]] = 1
+                        self.movements_to_right_v2[left[i].name] = 1
                     lista[k] = right[j]
                     j += 1
                 k += 1
@@ -39,4 +39,4 @@ class SortService:
                 j += 1
                 k += 1
 
-        return self.movements_to_right
+        return self.movements_to_right_v2
--- test/test_sort_service.py
+++ test/test_sort_service.py
@@ -1,24 +1,25 @@
 from unittest import TestCase
 
 from rank.sort_service import SortService
+from test.player_record_factory import instance_one
 
 
 class Test(TestCase):
 
-    def test_merge_sort_with_should_work_ok(self):
+    def test_merge_sort_with_should_work_ok_v2(self):
         # given
-        list = [3, 4, 2, 8, 6, 5]
+        list = instance_one()
         service = SortService()
 
         # when
-        improvements = service.all_improvements_by_previous_position(list)
+        improvements = service.all_improvements_by_previous_position_v2(list)
 
-        #3
+        # 3
         # then
-        self.assertTrue(1 == improvements.get(3))
-        self.assertTrue(1 == improvements.get(4))
-        self.assertTrue(1 == improvements.get(6))
-        self.assertTrue(2 == improvements.get(8))
+        self.assertTrue(1 == improvements.get("A"))
+        self.assertTrue(1 == improvements.get("B"))
+        self.assertTrue(1 == improvements.get("E"))
+        self.assertTrue(2 == improvements.get("D"))
 
 
 
